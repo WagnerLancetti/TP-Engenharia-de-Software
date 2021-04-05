688335c34d40b14f0f5d2745bf10aa65cdfd71b4
WagnerLancetti <56642195+WagnerLancetti@users.noreply.github.com>
2021-04-05 12:51:30 -0300

    Delete ControleChefe.py

--- ControleChefe.py
+++
@@ -1,9 +0,0 @@
-import Chefe as Chefe
-
-class ControleProfessor():
-    def __init__(self):
-        self.Chefe = Chefe.Chefe()
-    
-    # Recebe os dados para registrar uma atividade
-    def ValidarAtividades(self):
-        criacaoDoTrabalho = True
