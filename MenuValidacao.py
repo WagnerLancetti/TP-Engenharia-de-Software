89bb3940c255e77bf3baf40318927870393420c6
WagnerLancetti <56642195+WagnerLancetti@users.noreply.github.com>
2021-04-05 19:41:30 -0300

    Delete MenuValidacao.py

--- MenuValidacao.py
+++
@@ -1,8 +0,0 @@
-
-class MenuValidacao:
-    def __init__(self):
-        self.corBack = "#e9e9e9"
-        self.fontePadrao = ("Arial", "10")
-        self.menuVal = True
-    def menu(self, sistema):
-        self.sistema = sistema
