3319c967e0267e6fab780286810dbcf77d4c4cfa
WagnerLancetti <56642195+WagnerLancetti@users.noreply.github.com>
2021-04-05 12:51:35 -0300

    Delete ControleProfessor.py

--- ControleProfessor.py
+++
@@ -1,9 +0,0 @@
-import Professor as Professor
-
-class ControleProfessor():
-    def __init__(self):
-        self.Professor = Professor.Professor()
-    
-    # Recebe os dados para registrar uma atividade
-    def RegistrarAtividades(self):
-        criacaoDoTrabalho = True
